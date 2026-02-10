ad3f660aaf942ee2133d00389b4a9bd5a4124a45
Prit <pritmeghapata2516@gmail.com>
2026-02-10 21:49:32 +0530

    Refactor code structure for improved readability and maintainability

--- services/pdf/pdf_to_word_service.py
+++ services/pdf/pdf_to_word_service.py
@@ -2,54 +2,21 @@ import subprocess
 import tempfile
 import os
 import shutil
-
+from pdf2docx import Converter
 from core.config import get_soffice_path
 
-
 def pdf_to_word(input_file):
-    soffice = get_soffice_path()
-
     with tempfile.TemporaryDirectory() as tmpdir:
-        # write uploaded PDF
         pdf_path = os.path.join(tmpdir, "input.pdf")
+        docx_path = os.path.join(tmpdir, "output.docx")
+
         with open(pdf_path, "wb") as f:
             f.write(input_file.read())
 
-        # LibreOffice command
-        command = [
-            soffice,
-            "--headless",
-            "--nologo",
-            "--nofirststartwizard",
-            "--convert-to",
-            "docx",
-            pdf_path,
-            "--outdir",
-            tmpdir
-        ]
+        cv = Converter(pdf_path)
+        cv.convert(docx_path)
+        cv.close()
 
-        result = subprocess.run(
-            command,
-            stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE,
-            text=True
-        )
-
-        if result.returncode != 0:
-            raise RuntimeError(
-                f"LibreOffice failed\nSTDOUT:\n{result.stdout}\nSTDERR:\n{result.stderr}"
-            )
-
-        # 🔑 find actual output file
-        outputs = [f for f in os.listdir(tmpdir) if f.endswith(".docx")]
-        if not outputs:
-            raise RuntimeError(
-                f"LibreOffice did not create DOCX\nSTDERR:\n{result.stderr}"
-            )
-
-        docx_path = os.path.join(tmpdir, outputs[0])
-
-        # load output to memory
         output = tempfile.SpooledTemporaryFile()
         with open(docx_path, "rb") as f:
             shutil.copyfileobj(f, output)
